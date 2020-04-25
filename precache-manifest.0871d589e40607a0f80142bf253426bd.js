370f7483097410195f96feb3550620be4da24edd
Luis <luis@MacBook-Pro-de-Admin.local>
2020-04-24 22:02:31 -0500

    Updates

---
+++ precache-manifest.0871d589e40607a0f80142bf253426bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9dea9128456af96e331783eb8f08cae",
+    "url": "/monsters-react/index.html"
+  },
+  {
+    "revision": "b612b5fbdbfb5eca8957",
+    "url": "/monsters-react/static/css/main.d2a1f325.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-react/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-react/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b612b5fbdbfb5eca8957",
+    "url": "/monsters-react/static/js/main.e59dda7d.chunk.js"
+  },
+  {
+    "revision": "bb10d4d4a0632a4e8caf",
+    "url": "/monsters-react/static/js/runtime-main.2624874f.js"
+  }
+]);
